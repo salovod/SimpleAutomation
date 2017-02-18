d8624e405905381c90d635261a8244757836b792
ruslan.bilyi <beliysacred@gmail.com>
2017-02-18 17:40:00 +0200

    basic changes for gradle run

--- src/test/java/preparation/DriverConfiguration.java
+++ src/test/java/preparation/DriverConfiguration.java
@@ -1,7 +1,27 @@
 package preparation;
 
+
+import org.testng.annotations.BeforeMethod;
+
+import javax.security.auth.login.Configuration;
+
 /**
  * Created by Sacred on 18.02.2017.
  */
 public class DriverConfiguration {
+
+    private final int timeOut = 15000;
+    private final String browserName = "chrome";
+    protected SignUp signUp = new SignUp();
+
+    @BeforeMethod
+    public void before() {
+        ChromeDriverManager.getInstance().setup();
+        FirefoxDriverManager.getInstance().setup();
+        Configuration.startMaximized = true;
+        Configuration.browser = browserName;
+        Configuration.timeout = timeOut;
+        open(ENV);
+    }
+
 }
