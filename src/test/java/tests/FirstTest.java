1616b8369dc713a6d8a132a809b4ee7cad246e72
ruslan.bilyi <beliysacred@gmail.com>
2017-02-18 19:04:47 +0200

    header Rules and login

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import pages.common.Header;
 import preparation.DriverConfiguration;
 
 /**
@@ -14,6 +15,8 @@ public class FirstTest extends DriverConfiguration {
     public void firstTest() throws InterruptedException {
         LoginPage loginPage = new LoginPage();
         loginPage.loginToAccount();
+        Header header = new Header();
+        header.selectHeader("");
         Thread.sleep(3000);
     }
 
