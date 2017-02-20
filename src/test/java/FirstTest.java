870deb56f1bf6d83cd37904808d259338f26e04a
ruslan.bilyi <beliysacred@gmail.com>
2017-02-20 19:18:34 +0200

    removed gradle -> now maven

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.common.Header;
+import preparation.DriverConfiguration;
+
+/**
+ * Created by Sacred on 18.02.2017.
+ */
+public class FirstTest extends DriverConfiguration {
+
+
+    @Test
+    public void firstTest() throws InterruptedException {
+        LoginPage loginPage = new LoginPage();
+        loginPage.loginToAccount();
+        Header header = new Header();
+        header.selectHeader("");
+        Thread.sleep(3000);
+    }
+
+    //TODO make login -> OP && EC
+}
