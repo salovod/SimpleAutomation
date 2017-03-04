e114015f9471c6b31c44369232d77e1e7b756bae
ruslan.bilyi <beliysacred@gmail.com>
2017-03-04 18:32:18 +0200

    return amount of available resource buildings

---
+++ src/main/java/pages/villages/VillagePage.java
@@ -0,0 +1,43 @@
+package pages.villages;
+
+import com.codeborne.selenide.ElementsCollection;
+import rules.PageRules;
+
+import static com.codeborne.selenide.Selenide.$$;
+
+/**
+ * Created by Sacred on 18.02.2017.
+ */
+public class VillagePage extends PageRules {
+
+
+    private int returnAmountOfResourceFactories(String resource) {
+        ElementsCollection getResource = $$("[alt*='" + resource + "'");
+        int resourceSize = getResource.size();
+        return resourceSize;
+    }
+
+    public int getAmountOfFarms() {
+        return returnAmountOfResourceFactories("Ферма");
+    }
+
+    public int getAmountOfWood() {
+
+        return returnAmountOfResourceFactories("Лесопилка");
+    }
+
+    public int getAmountOfIron() {
+        return returnAmountOfResourceFactories("Железный");
+    }
+
+    public int getAmountOfCrop() {
+        return returnAmountOfResourceFactories("Глиняный");
+    }
+
+    public void getAllAmount() {
+        getAmountOfFarms();
+        getAmountOfWood();
+        getAmountOfIron();
+        getAmountOfCrop();
+    }
+}
