be1e4535928ac3dfafb39004dd541c08b1d7a33b
ruslan.bilyi <beliysacred@gmail.com>
2017-03-18 21:03:47 +0200

    reformat

--- src/main/java/pages/buy/Building.java
+++ src/main/java/pages/buy/Building.java
@@ -3,14 +3,14 @@ package pages.buy;
 import com.codeborne.selenide.SelenideElement;
 import pages.enums.Buildings;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 /**
  * Created by Sacred on 18.03.2017.
  */
 public class Building implements Buildings {
 
-    private SelenideElement wall = $$("#clickareas > area:nth-child(22)").get(0);
     private SelenideElement upgradeBuildingButton = $(".section1 > [type='button']");
 
     public void navigateToWallPage() {
--- src/main/java/pages/buy/Soldiers.java
+++ src/main/java/pages/buy/Soldiers.java
@@ -15,49 +15,50 @@ import static com.codeborne.selenide.Selenide.open;
  * Created by Sacred on 18.03.2017.
  */
 public class Soldiers extends Resources {
-	HashMap<RESOURCETYPE, Integer> neededResources = new HashMap<>();
-	private SelenideElement cropWareHouseLocator = $("#stockBarGranary");
-	private SelenideElement barWareHouseLocator = $("#stockBarWarehouse");
-	private SelenideElement wood = $(".trainUnits .r1"); //TODO check '.unit.u1' as a possible to select 'legioner' solder after adding new solder type
-	private SelenideElement clay = $(".trainUnits .r2");
-	private SelenideElement iron = $(".trainUnits .r3");
-	private SelenideElement crop = $(".trainUnits .r4");
-	private SelenideElement number = $("[name='t1']");
-	private SelenideElement amount = $(".details > a");
+    HashMap<RESOURCETYPE, Integer> neededResources = new HashMap<>();
+    private SelenideElement cropWareHouseLocator = $("#stockBarGranary");
+    private SelenideElement barWareHouseLocator = $("#stockBarWarehouse");
+    private SelenideElement wood = $(".trainUnits .r1"); //TODO check '.unit.u1' as a possible to select 'legioner' solder after adding new solder type
+    private SelenideElement clay = $(".trainUnits .r2");
+    private SelenideElement iron = $(".trainUnits .r3");
+    private SelenideElement crop = $(".trainUnits .r4");
+    private SelenideElement number = $("[name='t1']");
+    private SelenideElement amount = $(".details > a");
 
-	public void navigateToBarak() {
-		open(Buildings.V1_BARRACKS);
-	}
+    public void navigateToBarak() {
+        open(Buildings.V1_BARRACKS);
+    }
 
-	private void getNeededResources(SOLDERTYPE soldertype) {
-		neededResources.put(RESOURCETYPE.CROP,Integer.parseInt(crop.getText()));
-		neededResources.put(RESOURCETYPE.WOOD,Integer.parseInt(wood.getText()));
-		neededResources.put(RESOURCETYPE.IRON,Integer.parseInt(iron.getText()));
-		neededResources.put(RESOURCETYPE.CLAY,Integer.parseInt(clay.getText()));
+    private void getNeededResources(SOLDERTYPE soldertype) {
+        neededResources.put(RESOURCETYPE.CROP, Integer.parseInt(crop.getText()));
+        neededResources.put(RESOURCETYPE.WOOD, Integer.parseInt(wood.getText()));
+        neededResources.put(RESOURCETYPE.IRON, Integer.parseInt(iron.getText()));
+        neededResources.put(RESOURCETYPE.CLAY, Integer.parseInt(clay.getText()));
 
-	}
+    }
 
-	private boolean possibility(SOLDERTYPE soldertype, int count) { //TODO create check available resources and needed for current solder type-----
-		getNeededResources(SOLDERTYPE.LEGIONARE);
-		getAvailableResources();
-		for (RESOURCETYPE resourcetype : RESOURCETYPE.values()){
-			if (neededResources.get(resourcetype)*count > getAvailableResource(resourcetype))
-				return false;
-		}
-		return true;
-	}
+    private boolean possibility(SOLDERTYPE soldertype, int count) { //TODO create check available resources and needed for current solder type-----
+        getNeededResources(SOLDERTYPE.LEGIONARE);
+        getAvailableResources();
+        for (RESOURCETYPE resourcetype : RESOURCETYPE.values()) {
+            if (neededResources.get(resourcetype) * count > getAvailableResource(resourcetype))
+                return false;
+        }
+        return true;
+    }
 
-	private Integer getSolderAmount(SOLDERTYPE soldertype) {
-		return Integer.parseInt(amount.getText().replaceAll("[^0-9]",""));
-	}
+    private Integer getSolderAmount(SOLDERTYPE soldertype) {
+        return Integer.parseInt(amount.getText().replaceAll("[^0-9]", ""));
+    }
 
-	public void createSolders( SOLDERTYPE soldertype, int count){
-		if (possibility(soldertype, count)) {
-			if (getSolderAmount(soldertype) < count) {
-				count = getSolderAmount(soldertype);
-				System.out.print("Create " + count + " " + soldertype);
+    public void createSolders(SOLDERTYPE soldertype, int count) {
+        if (possibility(soldertype, count)) {
+            if (getSolderAmount(soldertype) < count) {
+                count = getSolderAmount(soldertype);
+                System.out.print("We can create " + count + " " + soldertype);
 //			number.val(String.valueOf(count)).pressEnter(); // temporary disable.
-			}
-		}
-	}
+            }
+        }
+    }
 }
+
