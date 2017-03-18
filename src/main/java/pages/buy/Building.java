afdd51c61c1b41be8bac85c3e98bb65fe4199eea
ruslan.bilyi <beliysacred@gmail.com>
2017-03-18 19:02:38 +0200

    interface for building

--- src/main/java/pages/buy/Building.java
+++ src/main/java/pages/buy/Building.java
@@ -1,17 +1,23 @@
 package pages.buy;
 
 import com.codeborne.selenide.SelenideElement;
+import pages.enums.Buildings;
 
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 /**
  * Created by Sacred on 18.03.2017.
  */
-public class Building {
+public class Building implements Buildings {
 
     private SelenideElement wall = $$("#clickareas > area:nth-child(22)").get(0);
+    private SelenideElement upgradeBuildingButton = $(".section1 > [type='button']");
 
-    public void clickOnWall() {
-        wall.click();
+    public void navigateToWallPage() {
+        open(V1_WALL);
+    }
+
+    public void setBuildWall() {
+        upgradeBuildingButton.click();
     }
 }
---
+++ src/main/java/pages/enums/Buildings.java
@@ -0,0 +1,13 @@
+package pages.enums;
+
+/**
+ * Created by Sacred on 18.03.2017.
+ */
+public interface Buildings {
+
+    String BUILD_URL = "http://ts2.travian.ru/build.php?id=";
+    String V1_WALL = BUILD_URL + "40";
+    String V1_ACADEMY = BUILD_URL + "24";
+    String V1_FORGE = BUILD_URL + "19";
+    String V1_BARRACKS = BUILD_URL + "27";
+}
